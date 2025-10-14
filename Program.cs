afa4c66470052bda685801d5928e550377961d3f
DouweBroekema <Douwe.broekema@gmail.com>
2025-10-14 17:00:54 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace ProgrammingLearningApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+            // Class diagram
+
+
+            //IAction
+            //methods: 
+            // Activate (Player _player)
+            //The following inherit from action.
+            //MoveAction : IAction
+            //TurnAction : IAction
+            //RepeatAction : IAction
+
+
+
+            // Player
+            //attribute:
+            // position Vector2
+            // facingDirection Vector2
+            //methods:
+            // movePlayer (Vector2 _movement = Vector2.Zero, Vector2 _direction = Vector2.Zero)
+
+
+            //FileReader
+            // methods:
+            // ReadFile (String _filePath)
+
+
+
+
+
+            
+        }
+    }
+}
