1a44c81daddcd6a78062d73c50a6aa90769fcd8d
DouweBroekema <Douwe.broekema@gmail.com>
2025-10-15 12:05:19 +0200

    Class diagram written out

--- Program.cs
+++ Program.cs
@@ -9,34 +9,55 @@
 
             // Class diagram
 
+            // GameManager
+            // Reads the inputs from the user and executes accordingly.
+            
 
+            // ProgramExecutor
+            // Takes parsed program and executes that program.
+            // methods:
+            // ExecuteProgram (Program _program) // perform all IActions.
+
+            // Program
+            // List<IAction>
+
+
+            // ProgramParser
+            // Program ReadAndParseProgram (string _FilePath)
+            // IAction ParseAction (string _newAction) ---> Returns new action based on string
+            // Read text file and turn into IAction list. Creates a new program.
+
+
+            // Here we implement the composite design pattern.
             //IAction
             //methods: 
             // Activate (Player _player)
             //The following inherit from action.
             //MoveAction : IAction
             //TurnAction : IAction
-            //RepeatAction : IAction
+            //RepeatAction : IAction ---> Will hold a list of IActions.  So thus could be more repeaters.
 
 
-
-            // Player
-            //attribute:
+            // Character
+            // attribute:
             // position Vector2
             // facingDirection Vector2
             //methods:
-            // movePlayer (Vector2 _movement = Vector2.Zero, Vector2 _direction = Vector2.Zero)
+            // moveCharacter (Vector2 _movement = Vector2.Zero, Vector2 _direction = Vector2.Zero)
 
 
-            //FileReader
-            // methods:
-            // ReadFile (String _filePath)
-
-
-
-
-
+            // MetricsManager
+            // CalculateAllMetrics (Program _program)
             
+
+            // RandomizedExampleProgram
+            // Attribute
+            // List<Program> examplePrograms;
+            // Program SelectRandomProgram()
+
+
+
+
         }
     }
 }
