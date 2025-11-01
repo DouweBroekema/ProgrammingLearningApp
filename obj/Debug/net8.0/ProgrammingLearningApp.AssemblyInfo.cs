1aa2ffca4e9a0ae2c9970cdba681884ff2980d48
DouweBroekema <Douwe.broekema@gmail.com>
2025-11-01 09:36:35 +0100

    Start

--- obj/Debug/net8.0/ProgrammingLearningApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProgrammingLearningApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProgrammingLearningApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcebe0ff1fb6492694cd3cd63ae30c81a7b938b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a862952e0ee48a558dc338b126b661a48e889dd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProgrammingLearningApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProgrammingLearningApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
